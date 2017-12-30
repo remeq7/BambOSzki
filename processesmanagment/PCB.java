a056aa391de9302a9c6d808d28423ec3aaa6a078
Szczepcio <34744054+Szczepcio@users.noreply.github.com>
2017-12-30 17:38:07 +0100

    Add files via upload

---
+++ processesmanagment/PCB.java
@@ -0,0 +1,27 @@
+package processesmanagment;
+
+//import java.util.HashMap;
+
+
+public class PCB {
+	
+	protected int ProcessID;
+	protected String ProcessName;
+	protected int ProcessState;
+	protected int BaseProcessPriority;
+	protected int CurrentProcessPriority;
+	protected boolean blocked;
+	protected int whenCameToList;
+	protected int howLongWaiting;
+	
+	
+	public String receivedMsg;
+	public int commandCounter;
+        public int howManyPages;
+	//public HashMap<String, Integer> labels = new HashMap<String, Integer>();
+	public int A;
+	public int B;
+	public int C;
+	public int D;
+
+}
